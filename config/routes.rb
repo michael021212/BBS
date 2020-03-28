25df65640c1fa4de1014d42d73a98a14d7e45690
tanakashun <michael021212@gmail.com>
2020-03-28 15:32:49 +0900

    [add]カテゴリ機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  # get 'posts/index'
-  # get 'posts/show'
-  # get 'posts/new'
+  get 'categories/index'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :posts, except: %i[edit update] do
     resources :comments, only: %i[create destroy]
+  end
+
+  resource :posts do
+    resources :categories, except: %i[index]
   end
 end
