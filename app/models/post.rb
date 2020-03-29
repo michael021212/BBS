264bec469bea6edbc848369772b22f0f47afcb2a
tanakashun <michael021212@gmail.com>
2020-03-29 11:07:38 +0900

    [update]カテゴリ表示メソッドを追加しviewから切り出し

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,9 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
   validates :nickname, presence: true, length: { maximum: 10 }
   validates :category_ids, presence: true
+
+  def convert_to_string_categories
+    category_names = categories.map(&:name)
+    category_names.join('/')
+  end
 end
