15c1dedd8161e90497fe54927fe30a31b9199b5f
tanakashun <michael021212@gmail.com>
2020-03-30 23:42:22 +0900

    [fix]バリデーション修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post, optional: true
 
-  validates :body, presence: true, length: { maximum: 30 }
+  validates :body, presence: true, length: { maximum: 200 }
   validates :nickname, presence: true, length: { maximum: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,6 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 30 }
   validates :nickname, presence: true, length: { maximum: 10 }
-  validates :category_ids, presence: true
 
   def convert_to_string_categories
     category_names = categories.map(&:name)
