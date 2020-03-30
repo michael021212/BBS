d99f3b1260b6ffe0ac37b0571e3f7da147105085
tanakashun <michael021212@gmail.com>
2020-03-30 17:55:05 +0900

    [add]commentsテーブルにindexを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_152448) do
+ActiveRecord::Schema.define(version: 2020_03_30_085037) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_03_27_152448) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
